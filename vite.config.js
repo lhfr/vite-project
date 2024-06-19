678d4ec244e287f402641074027b18e5a49e0f3d
white666 <8967008+whites666@user.noreply.gitee.com>
2024-06-19 13:37:43 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
 // https://vitejs.dev/config/
 export default defineConfig({
   // 部署到 github
-  base: "/vue-project/",
+  base: "/vite-project/",
   build: {
     outDir: "docs",
   },
